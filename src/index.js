0066f0745251cb8e52d768703e7c7a841e63ab63
ksc4130 <ksc4130@gmail.com>
2016-12-18 13:36:52 -0500

    last longer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import gpio from 'gpio';
+var gpio = require('gpio');
 
-var gpio22, gpio4, intervalTimer;
+var gpio22, gpio21, intervalTimer;
  
 // Flashing lights if LED connected to GPIO22 
 gpio22 = gpio.export(22, {
@@ -12,11 +12,11 @@ gpio22 = gpio.export(22, {
    }
 });
 
-gpio4 = gpio.export(4, {
+gpio21 = gpio.export(21, {
    ready: function() {
       // bind to gpio22's change event 
       gpio22.on("change", function(val) {
-         gpio4.set(1 - val); // set gpio4 to the opposite value 
+         gpio21.set(1 - val); // set gpio4 to the opposite value 
       });
    }
 });
@@ -27,10 +27,10 @@ setTimeout(function() {
    gpio22.reset();                        // sets header to low 
    gpio22.unexport();                     // unexport the header 
    
-   gpio4.unexport(function() {
+   gpio21.unexport(function() {
       // unexport takes a callback which gets fired as soon as unexporting is done 
       process.exit(); // exits your node program 
    });
-}, 60000);
+}, 120000);
 
 console.log('started');
