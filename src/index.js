bc4f3ef55508f7e7f7623b2d7bbb3c6da19294af
ksc4130 <ksc4130@gmail.com>
2016-12-18 23:24:05 -0500

    watch

--- src/index.js
+++ src/index.js
@@ -14,11 +14,20 @@ if(!fs.existsSync(analogPath + 'in_voltage1_raw')) {
 }
 
 function pullVal () {
-    let adcVal = Number(fs.readFileSync('/sys/bus/iio/devices/iio\:device0/in_voltage1_raw') + '');
-    let mV = adcVal / 4096 * 1800;
-    let celsius = (mV - 500) / 10;
-    let fahrenheit = (celsius * 9 / 5) + 32;
-    console.log(adcVal, fahrenheit);
+    fs.watchFile(analogPath + 'in_voltage1_raw', (curr, prev) => {
+        console.log(`the current mtime is: ${curr.mtime}`);
+        console.log(`the previous mtime was: ${prev.mtime}`);
+        let adcVal = Number(fs.readFileSync('/sys/bus/iio/devices/iio\:device0/in_voltage1_raw') + '');
+        let mV = adcVal / 4096 * 1800;
+        let celsius = (mV - 500) / 10;
+        let fahrenheit = (celsius * 9 / 5) + 32;
+        console.log(adcVal, fahrenheit);
+    });
+    // let adcVal = Number(fs.readFileSync('/sys/bus/iio/devices/iio\:device0/in_voltage1_raw') + '');
+    // let mV = adcVal / 4096 * 1800;
+    // let celsius = (mV - 500) / 10;
+    // let fahrenheit = (celsius * 9 / 5) + 32;
+    // console.log(adcVal, fahrenheit);
 }
 
 
