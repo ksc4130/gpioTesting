1b598c8f937d2bf6e52cdd61acd17af365d6432c
ksc4130 <ksc4130@gmail.com>
2016-12-19 12:14:39 -0500

    using gpio lib

--- src/index.js
+++ src/index.js
@@ -26,34 +26,54 @@ function init () {
     });
 }
 
-fs.exists(gpioPath + 'gpio54', exists => {
-    if(exists) {
-        fs.writeFile(gpioPath + 'unexport', '54', err => {
-            if(err)
-                return console.log(err);
-             exportGpio(54);   
-        });
-    } else {
-        exportGpio(54); 
-    }
+var gpio = require("gpio");
+
+// Calling export with a pin number will export that header and return a gpio header instance
+var gpio4 = gpio.export(54, {
+   // When you export a pin, the default direction is out. This allows you to set
+   // the pin value to either LOW or HIGH (3.3V) from your program.
+   direction: 'out',
+
+   // set the time interval (ms) between each read when watching for value changes
+   // note: this is default to 100, setting value too low will cause high CPU usage
+   interval: 200,
+
+   // Due to the asynchronous nature of exporting a header, you may not be able to
+   // read or write to the header right away. Place your logic in this ready
+   // function to guarantee everything will get fired properly
+   ready: function() {
+       gpio4.set(1);
+   }
 });
 
+// fs.exists(gpioPath + 'gpio54', exists => {
+//     if(exists) {
+//         fs.writeFile(gpioPath + 'unexport', '54', err => {
+//             if(err)
+//                 return console.log(err);
+//              exportGpio(54);   
+//         });
+//     } else {
+//         exportGpio(54); 
+//     }
+// });
 
 
-function exportGpio (n) {
-    fs.writeFile(gpioPath + 'export', '54', err => {
-        if(err)
-            return console.log('export error', err);
-        fs.writeFile(gpioPath + 'gpio' + n + '/direction', 'out', err => {
-            if(err)
-                return console.log('direction eroor', err);
-            fs.writeFile(gpioPath + 'gpio' + n + '/value', '1', err => {
-                if(err)
-                    return console.log('write failed', err);
-        });
-        });
-    });
-}
+
+// function exportGpio (n) {
+//     fs.writeFile(gpioPath + 'export', '54', err => {
+//         if(err)
+//             return console.log('export error', err);
+//         fs.writeFile(gpioPath + 'gpio' + n + '/direction', 'out', err => {
+//             if(err)
+//                 return console.log('direction eroor', err);
+//             fs.writeFile(gpioPath + 'gpio' + n + '/value', '1', err => {
+//                 if(err)
+//                     return console.log('write failed', err);
+//         });
+//         });
+//     });
+// }
 
 // function init () {
 //     console.log('watching');
